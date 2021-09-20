52e764f0614df4344321cad05fcc20b8fb16c777
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-09-20 19:47:20 +0300

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: ["airbnb", "airbnb-typescript"],
+    parserOptions: {
+        project: ["./tsconfig.json"]
+    }
+};
