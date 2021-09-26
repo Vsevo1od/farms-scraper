f41cfbf9142df1138ae3d85b9e221d8a39482605
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-09-26 15:18:59 +0300

    Add gem + trusted stable type

--- src/App/rows/getCoinsTypes.ts
+++ src/App/rows/getCoinsTypes.ts
@@ -4,13 +4,32 @@ function isCoinPairIncludesOnlyCoinsFromList(
   coin1: string,
   coin2: string | undefined,
   coinsList: string[] | readonly string[],
-) {
+): boolean {
   const isCoin1InList = coinsList.includes(coin1);
   if (!coin2) {
     return isCoin1InList;
   }
   const isCoin2InList = coinsList.includes(coin2);
   return isCoin1InList && isCoin2InList;
+}
+
+function isCoinPairACombinationOfLists(
+  coin1: string,
+  coin2: string | undefined,
+  coinList1: readonly string[] | string[],
+  coinList2: readonly string[] | string[],
+): boolean {
+  if (!coin2) {
+    return false;
+  }
+
+  const isCoin1InList1 = coinList1.includes(coin1);
+  const isCoin1InList2 = coinList2.includes(coin1);
+
+  const isCoin2InList1 = coinList1.includes(coin2);
+  const isCoin2InList2 = coinList2.includes(coin2);
+
+  return (isCoin1InList1 && isCoin2InList2) || (isCoin1InList2 && isCoin2InList1);
 }
 
 export default function getCoinsType(coin1: string, coin2?: string): string[] {
@@ -20,6 +39,9 @@ export default function getCoinsType(coin1: string, coin2?: string): string[] {
   }
   if (isCoinPairIncludesOnlyCoinsFromList(coin1, coin2, TRUSTED_STABLES)) {
     result.push('🛡️💲');
+  }
+  if (isCoinPairACombinationOfLists(coin1, coin2, GEMS, TRUSTED_STABLES)) {
+    result.push('💎+🛡️💲');
   }
 
   return result;
