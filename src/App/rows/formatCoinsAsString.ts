182e38cef658adaa68d9790c0b981162865476c8
vsevolod <5249361+Vsevo1od@users.noreply.github.com>
2021-09-26 15:25:42 +0300

    Make coin names uppercased

--- src/App/rows/formatCoinsAsString.ts
+++ src/App/rows/formatCoinsAsString.ts
@@ -1,10 +1,11 @@
 function formatCoinsAsString(coin1: string, coin2?: string): string {
   if (!coin2) {
-    return coin1;
+    return coin1.toUpperCase();
   }
 
   const isCoin1NameLessThanCoin2Name = coin1.localeCompare(coin2) <= 0;
-  return isCoin1NameLessThanCoin2Name ? `${coin1}/${coin2}` : `${coin2}/${coin1}`;
+  const lowerCaseResult = isCoin1NameLessThanCoin2Name ? `${coin1} / ${coin2}` : `${coin2} / ${coin1}`;
+  return lowerCaseResult.toUpperCase();
 }
 
 export default formatCoinsAsString;
